d106485dd911ad9190d1cca4c2f8f71114849589
Rey <reymainmail@gmail.com>
2022-01-16 08:52:19 -0800

    Added Blink cooldown

--- core-common/src/main/java/club/mineplex/core/mineplex/champions/skill/assassin/Blink.java
+++ core-common/src/main/java/club/mineplex/core/mineplex/champions/skill/assassin/Blink.java
@@ -1,12 +1,18 @@
 package club.mineplex.core.mineplex.champions.skill.assassin;
 
 import club.mineplex.core.mineplex.champions.ChampionsSkill;
+import club.mineplex.core.mineplex.champions.shop.IRechargeable;
 
-public class Blink extends ChampionsSkill {
+public class Blink extends ChampionsSkill implements IRechargeable {
 
     protected Blink() {
         super("Blink", Type.AXE, 4);
         this.setSubSkills(new SubSkill[]{new DeBlink()});
+    }
+
+    @Override
+    public double getRecharge(final int level) {
+        return 12.0D;
     }
 
     public static class DeBlink extends SubSkill {
